48ad2575ab692abee07e1287814aad5fb3a31f15
Craig Fletcher <hi@leakypixel.net>
2019-01-09 17:13:41 +0000

    Update args

--- snapcastrd.sh
+++ snapcastrd.sh
@@ -1,3 +1,3 @@
 cd /root/snapcastr/
 sed -i "s/server_addr='innocence'/server_addr='$SNAPCAST_HOST'/g" snapcastr/snapcastr.py
-PYTHONPATH=. python ./bin/snapcastrd --host=0.0.0.0 --port=5011
+PYTHONPATH=. python ./bin/snapcastrd --bind=0.0.0.0 --port=5011
