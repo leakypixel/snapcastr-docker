f3ab3f02b0c3b5bccbc30e2cef6f2869503ad923
Philippe <onebudwazer@gmail.com>
2020-11-21 00:35:57 +0100

    Update snapcastrd.sh
    update right snapcast host

--- snapcastrd.sh
+++ snapcastrd.sh
@@ -1,3 +1,3 @@
 cd /root/snapcastr/
-sed -i "s/server_addr='innocence'/server_addr='$SNAPCAST_HOST'/g" snapcastr/snapcastr.py
+sed -i "s/start_server.addr='localhost'/start_server.addr='$SNAPCAST_HOST'/g" snapcastr/snapcastr.py
 PYTHONPATH=. python ./bin/snapcastrd --bind=0.0.0.0 --port=5011
